5f6516be89bba88595528aafc67a9b5436fd24b1
Carlos Ramírez <cadavid4003@gmail.com>
2021-11-30 21:59:17 -0500

    first API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,23 @@ import Header from "./components/Header"
 import Search from "./components/Search"
 import "./appStyle.css"
 
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY
+
 function App() {
   const [word, setWord] = useState("")
+
   const handleSearchSubmit = (event) => {
     event.preventDefault()
-    console.log(word)
+    fetch(
+      `https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`
+    )
+      .then((res) => res.json()) //code executed when promise is resolved
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
   }
   return (
     <div className="App">
