1020de5e9d15400d1fdfae2127157c4a8cfc8ffb
Carlos Ramírez <cadavid4003@gmail.com>
2021-12-17 18:34:41 -0500

    added dockerfile for the api service

--- api/main.py
+++ api/main.py
@@ -25,6 +25,7 @@ def new_image():
     word = request.args.get("query")
     headers = {"Accept-Version": "v1", "Authorization": "Client-ID " + UNSPLASH_KEY}
     params = {"query": word}
+
     response = requests.get(url=UNSPLASH_URL, headers=headers, params=params)
     data = response.json()
     return data
